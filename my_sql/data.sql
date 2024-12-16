8372dec1797fc1ae9b3c68672600c6b0d704624b
Majda-Fg <majda.fougou@gmail.com>
2024-12-16 01:40:08 +0100

    étape 2

--- my_sql/data.sql
+++ my_sql/data.sql
@@ -5,3 +5,21 @@ CREATE TABLE edusign (
     email TEXT NOT NULL UNIQUE,
     sign_at DATETIME NOT NULL
 )
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
+)
+
+DROP TABLE edusign
+
+CREATE TABLE edusign (
+    user_id INTEGER REFERENCES users(id),
+    sign_at DATETIME NOT NULL
+)
+
+INSERT INTO users (firstname, lastname, email) VALUES ('Ada', 'Lovelace', 'ada@test.fr');
+INSERT INTO users (firstname, lastname, email) VALUES ('Beatrice', 'Worsley', 'bea@test.fr');
+INSERT INTO users (firstname, lastname, email) VALUES ('Bella', 'Guerin', 'bella@test.fr');
+INSERT INTO users (firstname, lastname, email) VALUES ('Barbara', 'Chase', 'barbara@test.fr');
